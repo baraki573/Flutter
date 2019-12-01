ef89b213b117933cf2d87945a3b10911d37633df
Mo <mohammadrahim.masoumi@stud.tu-darmstadt.de>
2019-12-01 18:08:06 +0100

    Added bottom navigation bar

---
+++ lib/bottom_navigationbar/navigationbar_pattern.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:museum_app/bottom_navigationbar/animated_navigationbar.dart';
+
+class BottomBarNavigationPatternExample extends StatefulWidget {
+  final List<BarItem> barItems = [
+    BarItem(
+      text: "Home",
+      iconData: Icons.home,
+      color: Colors.indigo,
+    ),
+    BarItem(
+      text: "Tours",
+      iconData: Icons.assistant_photo,
+      color: Colors.pinkAccent,
+    ),
+    BarItem(
+      text: "Add",
+      iconData: Icons.add,
+      color: Colors.yellow.shade900,
+    ),
+    BarItem(
+      text: "Profile",
+      iconData: Icons.person_outline,
+      color: Colors.teal,
+    ),
+  ];
+
+  @override
+  _BottomBarNavigationPatternExampleState createState() =>
+      _BottomBarNavigationPatternExampleState();
+}
+
+class _BottomBarNavigationPatternExampleState
+    extends State<BottomBarNavigationPatternExample> {
+  int selectedBarIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedContainer(
+        color: widget.barItems[selectedBarIndex].color,
+        duration: const Duration(milliseconds: 300),
+      ),
+      bottomNavigationBar: AnimatedBottomBar(
+          barItems: widget.barItems,
+          animationDuration: const Duration(milliseconds: 150),
+          barStyle: BarStyle(
+            fontSize: 16.0,
+            iconSize: 25.0
+          ),
+          onBarTap: (index) {
+            setState(() {
+              selectedBarIndex = index;
+            });
+          }),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:museum_app/bottom_navigationbar/navigationbar_pattern.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BottomBarNavigationPatternExample(),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
