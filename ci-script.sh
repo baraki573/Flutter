4e836b11fcf76469c09225973e4e57811b1f8c63
Shayan <shayandavarifard@yahoo.com>
2019-12-02 17:57:12 +0100

    bug fixed ?

--- ci-script.sh
+++ ci-script.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 
 # Rename tmdb_config.dart.sample file so that the project compiles
-(cd core/lib/src && mv tmdb_config.dart.sample tmdb_config.dart)
+# (cd core/lib/src && mv tmdb_config.dart.sample tmdb_config.dart)
 
 # Get all packages for core, mobile and web
 (cd core && pub get)
