a11f1a28b1291a6e2f560d3dfd5a92de8168dc41
Shayan <shayandavarifard@yahoo.com>
2019-12-02 17:18:54 +0100

    CIDC added

---
+++ ci-script.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Rename tmdb_config.dart.sample file so that the project compiles
+(cd core/lib/src && mv tmdb_config.dart.sample tmdb_config.dart)
+
+# Get all packages for core, mobile and web
+(cd core && pub get)
+(cd mobile && flutter packages get)
+
+# Analyze core, mobile and web
+(cd core && dartanalyzer ./ --fatal-infos --fatal-warnings)
+(cd mobile && dartanalyzer ./ --fatal-infos --fatal-warnings)
+
+# Run tests for core, mobile and web
+echo "--- Running tests in core... ---"
+(cd core && pub run test)
+
+echo "--- Running tests in mobile... ---"
+(cd mobile && flutter test)
